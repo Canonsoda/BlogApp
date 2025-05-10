ffecf4d951618f4815c4f3851de205f659dc50fc
Canonsoda <aryankumardwivedi11@gmail.com>
2025-05-10 07:29:53 +0530

    Case Change yet again

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { AuthLayout, Login } from './components/index.js'
 
 
 import AddPost from "./pages/AddPost";
-import SignUp from './pages/Signup'
+import SignUp from './pages/SignUp'
 import EditPost from "./pages/EditPost";
 
 import Post from "./pages/Post";
